32e9aae2b34b972d15bf7b08ceeca68ba206cf6d
Victor Grigoriu <vgrigoriu@gmail.com>
2016-06-17 19:18:37 +0300

    Add Location properties

--- src/Hermes/Entities/Location.cs
+++ src/Hermes/Entities/Location.cs
@@ -2,5 +2,14 @@
 {
     public class Location
     {
+        public int LocationId { get; set; }
+
+        public string StreetAddress { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string City { get; set; }
+
+        public string StateProvince { get; set; }
     }
 }
