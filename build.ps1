214afebdf527e3fa461e4eb79641fa914913cb76
Victor Grigoriu <vgrigoriu@gmail.com>
2016-06-20 19:23:30 +0300

    Be specific about which msbuild version we want

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 tools\nuget.exe restore src\Hermes\Hermes.sln
 
-msbuild src\Hermes\Hermes.sln
+&'C:\Program Files (x86)\MSBuild\14.0\bin\msbuild' src\Hermes\Hermes.sln
 
 src\Hermes\packages\roundhouse.0.8.6\bin\rh.exe --server `(localdb`)\v11.0 --database Hermes --versionfile=src\Hermes\Entities\bin\Debug\Entities.dll --files src\Hermes\db
