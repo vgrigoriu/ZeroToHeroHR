bfcc001b74502c6f9975da8ea3419b7f83e33d56
Victor Grigoriu <vgrigoriu@gmail.com>
2016-06-20 19:23:40 +0300

    Add Department data

---
+++ src/Hermes/db/runAfterOtherAnyTimeScripts/LOCAL.Insert Departments.ENV.sql
@@ -0,0 +1,161 @@
+INSERT INTO Department VALUES
+        ( 10
+        , 'Administration'
+        , 1700
+        );
+
+INSERT INTO Department VALUES
+        ( 20
+        , 'Marketing'
+        , 1800
+        );
+
+INSERT INTO Department VALUES
+        ( 30
+        , 'Purchasing'
+        , 1700
+        );
+
+INSERT INTO Department VALUES
+        ( 40
+        , 'Human Resources'
+        , 2400
+        );
+
+INSERT INTO Department VALUES
+        ( 50
+        , 'Shipping'
+        , 1500
+        );
+
+INSERT INTO Department VALUES
+        ( 60
+        , 'IT'
+        , 1400
+        );
+
+INSERT INTO Department VALUES
+        ( 70
+        , 'Public Relations'
+        , 2700
+        );
+
+INSERT INTO Department VALUES
+        ( 80
+        , 'Sales'
+        , 2500
+        );
+
+INSERT INTO Department VALUES
+        ( 90
+        , 'Executive'
+        , 1700
+        );
+
+INSERT INTO Department VALUES
+        ( 100
+        , 'Finance'
+        , 1700
+        );
+
+INSERT INTO Department VALUES
+        ( 110
+        , 'Accounting'
+        , 1700
+        );
+
+INSERT INTO Department VALUES
+        ( 120
+        , 'Treasury'
+        , 1700
+        );
+
+INSERT INTO Department VALUES
+        ( 130
+        , 'Corporate Tax'
+        , 1700
+        );
+
+INSERT INTO Department VALUES
+        ( 140
+        , 'Control And Credit'
+        , 1700
+        );
+
+INSERT INTO Department VALUES
+        ( 150
+        , 'Shareholder Services'
+        , 1700
+        );
+
+INSERT INTO Department VALUES
+        ( 160
+        , 'Benefits'
+        , 1700
+        );
+
+INSERT INTO Department VALUES
+        ( 170
+        , 'Manufacturing'
+        , 1700
+        );
+
+INSERT INTO Department VALUES
+        ( 180
+        , 'Construction'
+        , 1700
+        );
+
+INSERT INTO Department VALUES
+        ( 190
+        , 'Contracting'
+        , 1700
+        );
+
+INSERT INTO Department VALUES
+        ( 200
+        , 'Operations'
+        , 1700
+        );
+
+INSERT INTO Department VALUES
+        ( 210
+        , 'IT Support'
+        , 1700
+        );
+
+INSERT INTO Department VALUES
+        ( 220
+        , 'NOC'
+        , 1700
+        );
+
+INSERT INTO Department VALUES
+        ( 230
+        , 'IT Helpdesk'
+        , 1700
+        );
+
+INSERT INTO Department VALUES
+        ( 240
+        , 'Government Sales'
+        , 1700
+        );
+
+INSERT INTO Department VALUES
+        ( 250
+        , 'Retail Sales'
+        , 1700
+        );
+
+INSERT INTO Department VALUES
+        ( 260
+        , 'Recruiting'
+        , 1700
+        );
+
+INSERT INTO Department VALUES
+        ( 270
+        , 'Payroll'
+        , 1700
+        );
