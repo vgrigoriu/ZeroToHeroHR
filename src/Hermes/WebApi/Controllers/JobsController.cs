9f759766d542c46ee058dea80f43693608259a58
Victor Grigoriu <vgrigoriu@gmail.com>
2016-06-26 13:08:18 +0300

    Get job by id

--- src/Hermes/WebApi/Controllers/JobsController.cs
+++ src/Hermes/WebApi/Controllers/JobsController.cs
@@ -1,5 +1,6 @@
 ﻿using DatabaseAccessLayer;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace WebApi.Controllers
@@ -11,6 +12,21 @@ namespace WebApi.Controllers
             using (var db = new HRContext())
             {
                 return Ok(db.Jobs.ToList());
+            }
+        }
+
+        public async Task<IHttpActionResult> Get(string id)
+        {
+            using (var db = new HRContext())
+            {
+                var job = await db.Jobs.FindAsync(id);
+
+                if (job == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(job);
             }
         }
     }
