7ab095c7d72847ad63205fe3ad9f2eda71770253
Victor Grigoriu <vgrigoriu@gmail.com>
2016-06-26 12:07:14 +0300

    Add insert jobs script

---
+++ src/Hermes/db/runAfterOtherAnyTimeScripts/0003.LOCAL.Insert Jobs.ENV.sql
@@ -0,0 +1,129 @@
+INSERT INTO Job VALUES
+        ( 'AD_PRES'
+        , 'President'
+        , 20000
+        , 40000
+        );
+INSERT INTO Job VALUES
+        ( 'AD_VP'
+        , 'Administration Vice President'
+        , 15000
+        , 30000
+        );
+
+INSERT INTO Job VALUES
+        ( 'AD_ASST'
+        , 'Administration Assistant'
+        , 3000
+        , 6000
+        );
+
+INSERT INTO Job VALUES
+        ( 'FI_MGR'
+        , 'Finance Manager'
+        , 8200
+        , 16000
+        );
+
+INSERT INTO Job VALUES
+        ( 'FI_ACCOUNT'
+        , 'Accountant'
+        , 4200
+        , 9000
+        );
+
+INSERT INTO Job VALUES
+        ( 'AC_MGR'
+        , 'Accounting Manager'
+        , 8200
+        , 16000
+        );
+
+INSERT INTO Job VALUES
+        ( 'AC_ACCOUNT'
+        , 'Public Accountant'
+        , 4200
+        , 9000
+        );
+INSERT INTO Job VALUES
+        ( 'SA_MAN'
+        , 'Sales Manager'
+        , 10000
+        , 20000
+        );
+
+INSERT INTO Job VALUES
+        ( 'SA_REP'
+        , 'Sales Representative'
+        , 6000
+        , 12000
+        );
+
+INSERT INTO Job VALUES
+        ( 'PU_MAN'
+        , 'Purchasing Manager'
+        , 8000
+        , 15000
+        );
+
+INSERT INTO Job VALUES
+        ( 'PU_CLERK'
+        , 'Purchasing Clerk'
+        , 2500
+        , 5500
+        );
+
+INSERT INTO Job VALUES
+        ( 'ST_MAN'
+        , 'Stock Manager'
+        , 5500
+        , 8500
+        );
+INSERT INTO Job VALUES
+        ( 'ST_CLERK'
+        , 'Stock Clerk'
+        , 2000
+        , 5000
+        );
+
+INSERT INTO Job VALUES
+        ( 'SH_CLERK'
+        , 'Shipping Clerk'
+        , 2500
+        , 5500
+        );
+
+INSERT INTO Job VALUES
+        ( 'IT_PROG'
+        , 'Programmer'
+        , 4000
+        , 10000
+        );
+
+INSERT INTO Job VALUES
+        ( 'MK_MAN'
+        , 'Marketing Manager'
+        , 9000
+        , 15000
+        );
+
+INSERT INTO Job VALUES
+        ( 'MK_REP'
+        , 'Marketing Representative'
+        , 4000
+        , 9000
+        );
+
+INSERT INTO Job VALUES
+        ( 'HR_REP'
+        , 'Human Resources Representative'
+        , 4000
+        , 9000
+        );
+
+INSERT INTO Job VALUES
+        ( 'PR_REP'
+        , 'Public Relations Representative'
+        , 4500
+        , 10500
+        );
