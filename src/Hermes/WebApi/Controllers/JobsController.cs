f75d7e8411c40f3b0e15940f40f76b63821c2fd2
Victor Grigoriu <vgrigoriu@gmail.com>
2016-06-26 12:55:32 +0300

    Add empty Web API project

---
+++ src/Hermes/WebApi/Controllers/JobsController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class JobsController : ApiController
+    {
+        public string Get()
+        {
+            return "cucu bau";
+        }
+    }
+}
